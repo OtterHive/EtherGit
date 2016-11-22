2941cc6cab963bfda0dae299925742a1b88afb83
Soren Peterson <soren468@gmail.com>
2016-11-22 14:53:55 -0600

    adapt git-remote-ether from git-remote-mango

--- gitremote/git-remote-ether.js
+++ gitremote/git-remote-ether.js
@@ -1,1 +1,34 @@
 #!/usr/bin/env node
+/* global require */
+
+// This is adapted from here https://github.com/axic/git-remote-mango/blob/master/git-remote-mango
+
+const toPull = require('stream-to-pull-stream');
+const pull = require('pull-stream');
+const gitRemoteHelper = require('pull-git-remote-helper');
+const url = require('url');
+const process = require('process');
+
+const Repo = require('./repo.js');
+
+function die () {
+    console.error.apply(console, arguments);
+    process.exit(1);
+}
+
+var args = process.argv.slice(2);
+if (args.length < 2) {
+    die('Usage: git-remote-mango <name> <url>');
+}
+
+var repo = url.parse(args[1]);
+
+if (repo.protocol !== 'mango:') {
+    die('Invalid protocol');
+}
+
+pull(
+    toPull(process.stdin),
+    gitRemoteHelper(new Repo(repo.hostname, repo.auth)),
+    toPull(process.stdout)
+);
