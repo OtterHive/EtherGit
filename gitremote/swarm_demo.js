d956bc2970c64a726dd931e74ff370cc2b0d5f79
Soren Peterson <soren468@gmail.com>
2016-11-25 16:19:16 -0600

    depend on the swarm version of web3

---
+++ gitremote/swarm_demo.js
@@ -0,0 +1,46 @@
+//
+// Needs web3.js from https://github.com/axic/web3.js/tree/swarm
+//
+
+var Web3 = require('web3');
+var url = require('url');
+
+var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+try {
+    web3.eth.defaultAccount = web3.eth.coinbase;
+} catch (e) {
+}
+
+var swarmPut = function (buf, enc, cb) {
+    web3.bzz.put(buf, enc, function (err, ret) {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, ret);
+    });
+};
+
+var swarmGet = function (key, cb) {
+    web3.bzz.get('bzz://' + key, function (err, ret) {
+        if (err) {
+            return cb(err)
+        }
+        cb(null, ret.content)
+    })
+}
+
+swarmPut('Hello World', 'application/text', function (err, ret) {
+    if (err) {
+        console.log('Swarm put failed: ', err)
+        return
+    }
+    console.log('Added to swarm: ', ret)
+
+    swarmGet(ret, function (err, ret) {
+        if (err) {
+            console.log('Swarm get failed: ', err)
+            return
+        }
+        console.log('Retrieved from swarm: ', ret)
+    })
+});
