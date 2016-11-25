a228c69e76905d423e78e78883efcd59c115d278
Soren Peterson <soren468@gmail.com>
2016-11-24 21:21:21 -0600

    add modifiers to restrict the ref methods

--- contracts/Repository.sol
+++ contracts/Repository.sol
@@ -1,17 +1,43 @@
 pragma solidity ^0.4.6;
 
 contract Repository {
-    mapping (bytes32 => string) refs;
+    mapping (bytes32 => Ref) refs;
 
-    function createRef (bytes32 refname, string hash) {
-        refs[refname] = hash;
+    struct Ref {
+        address owner;
+        string hash;
     }
 
-    function updateRef (bytes32 refname, string hash) {
-        refs[refname] = hash;
+    modifier onlyNew (bytes32 refname) {
+        if (refs[refname].owner != 0x0) {
+            throw;
+        }
+        _;
     }
 
-    function deleteRef (bytes32 refname) {
+    modifier neverMaster (bytes32 refname) {
+        if (refname == 'master') {
+            throw;
+        }
+        _;
+    }
+
+    modifier onlyOwner (bytes32 refname) {
+        if (msg.sender != refs[refname].owner) {
+            throw;
+        }
+        _;
+    }
+
+    function createRef (bytes32 refname, string hash) neverMaster(refname) onlyNew(refname) {
+        refs[refname] = Ref(msg.sender, hash);
+    }
+
+    function updateRef (bytes32 refname, string hash) neverMaster(refname) onlyOwner(refname) {
+        refs[refname].hash = hash;
+    }
+
+    function deleteRef (bytes32 refname) neverMaster(refname) onlyOwner(refname) {
         delete refs[refname];
     }
 }
