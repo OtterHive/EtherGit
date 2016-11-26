1c5bf96f9d6f886212d6925e3e9256c5b9cb13b0
Soren Peterson <soren468@gmail.com>
2016-11-26 00:08:04 -0600

    still testing swarm demo

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM node
-COPY . /srv/app
 WORKDIR /srv/app
+COPY package.json /srv/app/package.json
 RUN npm install
+COPY . /srv/app
 CMD pwd && ls && node ./gitremote/swarm_demo.js
--- gitremote/swarm_demo.js
+++ gitremote/swarm_demo.js
@@ -5,7 +5,7 @@
 var Web3 = require('web3');
 var url = require('url');
 
-var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+var web3 = new Web3(new Web3.providers.HttpProvider('http://geth:8545'));
 try {
     web3.eth.defaultAccount = web3.eth.coinbase;
 } catch (e) {
@@ -29,18 +29,22 @@ var swarmGet = function (key, cb) {
     })
 }
 
-swarmPut('Hello World', 'application/text', function (err, ret) {
-    if (err) {
-        console.log('Swarm put failed: ', err)
-        return
-    }
-    console.log('Added to swarm: ', ret)
+for (var i = 0; i < 1000; i++) {
+    console.log(web3.eth.coinbase);
+}
 
-    swarmGet(ret, function (err, ret) {
-        if (err) {
-            console.log('Swarm get failed: ', err)
-            return
-        }
-        console.log('Retrieved from swarm: ', ret)
-    })
-});
+// swarmPut('Hello World', 'application/text', function (err, ret) {
+//     if (err) {
+//         console.log('Swarm put failed: ', err)
+//         return
+//     }
+//     console.log('Added to swarm: ', ret)
+//
+//     swarmGet(ret, function (err, ret) {
+//         if (err) {
+//             console.log('Swarm get failed: ', err)
+//             return
+//         }
+//         console.log('Retrieved from swarm: ', ret)
+//     })
+// });
