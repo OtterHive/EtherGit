cb66d0b5011ef830a81b37d150a838b6a50511a5
Soren Peterson <soren468@gmail.com>
2016-11-26 19:25:47 -0600

    checkout swarm in update.sh

--- server/update.sh
+++ server/update.sh
@@ -1,4 +1,4 @@
 cd $GOPATH/src/github.com/ethereum/go-ethereum
-git checkout master
+git checkout swarm
 git pull
-go build ./cmd/geth ./cmd/bzzd ./cmd/bzzup
+go build ./cmd/geth ./cmd/bzzd ./cmd/bzzup ./cmd/bzzhash
